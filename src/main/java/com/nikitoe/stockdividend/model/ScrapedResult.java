4971c70c274a9c9e1c374410adc6a55944f719fc
Jiyong Sim <sjy19910222@gmail.com>
2022-09-17 22:56:48 +0900

    chore : 모델 구현

---
+++ src/main/java/com/nikitoe/stockdividend/model/ScrapedResult.java
@@ -0,0 +1,20 @@
+package com.nikitoe.stockdividend.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ScrapedResult {
+
+    private Company company;
+
+    private List<Dividend> dividendEntities;
+
+    public ScrapedResult() {
+        this.dividendEntities = new ArrayList<>();
+    }
+
+}
